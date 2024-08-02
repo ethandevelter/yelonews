a8f493de5f220cee1fa349fe464373e73e4bca56
ethandevelter <ethan.develter@gmail.com>
2024-08-02 18:55:49 +0200

    Changes

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,22 +5,8 @@
         </a>
         <div class="top-menu ml-10">
             <div class="flex space-x-4">
-                {{--<li>
-                    <a class="flex space-x-2 items-center hover:text-yellow-900 text-sm text-yellow-500"
-                        href="http://127.0.0.1:8000">
-                        Home
-                    </a>
-                </li>--}}
                 <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
                     {{ __('Home') }}
-                </x-nav-link>
-
-                <x-nav-link href="{{ route('blog') }}" :active="request()->routeIs('blog')">
-                    {{ __('Blog') }}
-                </x-nav-link>
-
-                <x-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.index')">
-                    {{ __('Posting') }}
                 </x-nav-link>
             </div>
         </div>
